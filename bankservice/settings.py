5d788b91ef56bbe4b19a95f430a392418dee85fb
Никита Быстров <hallnik96@gmail.com>
2025-06-01 16:52:11 +0300

    security: hide django secret key

--- bankservice/settings.py
+++ bankservice/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m#5hgfyxi4%0k9ss_df)czormyn$z(wo-3lp_01!2#muf&w3f("
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', default=get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
